cf2139a18f6f49dd6a346a7a8a2ebb422cb5448d
skayakill <chairulrizky233@gmail.com>
2025-05-07 09:36:37 +0700

    update posts in web.php

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,12 +6,14 @@
   
 
   <article class="py-8 max-w-screen-md border-b border-gray-300">
-    <h2 class="mb-1 text-xl tracking-tight font-bold text-gray-800">Judul Article</h2>
+    <a href="/posts/{{ $post ['id'] }}">
+    <h2 class="mb-1 text-xl tracking-tight font-bold text-gray-800"> {{ $post ['title'] }}</h2>
+    </a>
     <div class=" text-gray-500">
-      <a href="#">Dimas ukin</a> | 2 Februari 2004
+      <a href="#">{{ $post ['Author'] }}</a> | 2 Februari 2004
     </div>
-    <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quibusdam magnam quasi doloribus, iste ex aliquid veritatis sint qui quae est explicabo, ea non necessitatibus? Repellendus adipisci aliquid atque dicta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi earum rerum sed? Velit laborum quasi qui, accusantium vero, dolorem cum adipisci minima, expedita voluptatibus vitae consequuntur neque placeat nulla a.</p>
-    <a href="" class="font-medium text-blue-400 hover:underline">Read more &raquo;</a>
+    <p class="my-4 font-light">{{ Str::limit( $post ['body'], 100) }}</p>
+    <a href="/posts/{{ $post['id'] }}" class="font-medium text-blue-400 hover:underline">Read more &raquo;</a>
   </article>
   @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,19 +8,45 @@ Route::get('/', function () {
 });
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => [
-        [
+        [   
+            'id' => 1,
             'title' => 'Judul Artikel 1',
             'Author' => 'Dimas ukin',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quibusdam magnam quasi doloribus, iste ex aliquid veritatis sint qui quae est explicabo, ea non necessitatibus? Repellendus adipisci aliquid atque dicta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi earum rerum sed? Velit laborum quasi qui, accusantium vero, dolorem cum adipisci minima, expedita voluptatibus vitae consequuntur neque placeat nulla a.'
              
         ],
         [
+            'id' => 2,
             'title' => 'Judul Artikel 2',
             'Author' => 'Dimas ukin',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vitae, aut quo asperiores numquam, alias illum necessitatibus quod minus voluptatem fugit rerum placeat cum dolor a labore quis earum quibusda Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut accusantium nostrum, quia sed ratione repellendus aliquam? Numquam, nemo iusto eaque repellendus vel laboriosam est dicta cum perferendis architecto reiciendis. Atque!.'
         ]
     ]]);
 });
+
+Route::get('/posts/{id}', function($id) {    
+     $posts =  [   
+        [
+        'id' => 1,
+        'title' => 'Judul Artikel 1',
+        'Author' => 'Dimas ukin',
+        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quibusdam magnam quasi doloribus, iste ex aliquid veritatis sint qui quae est explicabo, ea non necessitatibus? Repellendus adipisci aliquid atque dicta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi earum rerum sed? Velit laborum quasi qui, accusantium vero, dolorem cum adipisci minima, expedita voluptatibus vitae consequuntur neque placeat nulla a.'
+         
+    ],
+    [
+        'id' => 2,
+        'title' => 'Judul Artikel 2',
+        'Author' => 'Dimas ukin',
+        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vitae, aut quo asperiores numquam, alias illum necessitatibus quod minus voluptatem fugit rerum placeat cum dolor a labore quis earum quibusda Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut accusantium nostrum, quia sed ratione repellendus aliquam? Numquam, nemo iusto eaque repellendus vel laboriosam est dicta cum perferendis architecto reiciendis. Atque!.'
+    ]
+    ];
+    $post = Arr::first($posts, function( $post) use ($id){
+        return $post['id'] == $id;
+    });
+
+    return view('post', ['title'=> 'single post', 'post' => $post]);
+});
+
 Route::get('/about', function () {
     return view('about', ['title'=> 'About']);
 });
